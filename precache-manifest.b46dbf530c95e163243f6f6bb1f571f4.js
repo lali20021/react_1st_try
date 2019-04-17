20b56ae733909e437119815c1a4062f2b831b6d6
Lali Gabelaya <laligabelaya@gmail.com>
2019-04-16 19:31:59 -0700

    Updates

---
+++ precache-manifest.b46dbf530c95e163243f6f6bb1f571f4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f30264567836235703e1",
+    "url": "/react-tutorial/static/css/main.8815da3c.chunk.css"
+  },
+  {
+    "revision": "f30264567836235703e1",
+    "url": "/react-tutorial/static/js/main.0f24fbdf.chunk.js"
+  },
+  {
+    "revision": "2a4d04de927634e03e5b",
+    "url": "/react-tutorial/static/js/runtime~main.782a6e4d.js"
+  },
+  {
+    "revision": "3a9e33913cab0720716d",
+    "url": "/react-tutorial/static/js/2.86a2265c.chunk.js"
+  },
+  {
+    "revision": "05e50b7ba8d734b1f3ccef443e196ee6",
+    "url": "/react-tutorial/index.html"
+  }
+];
